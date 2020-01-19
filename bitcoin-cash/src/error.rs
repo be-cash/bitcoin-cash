4b2ad3b2fef7f92905eb0bdec9ffb3aceb74cb9e
tobiasruck <ruck.tobias@gmail.com>
2020-01-19 13:14:33 +0100

    Added new errors

--- bitcoin-cash/src/error.rs
+++ bitcoin-cash/src/error.rs
@@ -1,5 +1,34 @@
 use crate::address::CashAddrError;
-use crate::deserialize::BitcoinCodeError;
+
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum BitcoinCodeError {
+    DeserializeAnyNotSupported,
+    InvalidBoolEncoding(u8),
+    LeftoverBytes,
+    DataTypeNotSupported(&'static str),
+    MethodNotSupported(&'static str),
+    SequenceMustHaveLength,
+}
+
+impl BitcoinCodeError {
+    pub fn into_err<T>(self) -> Result<T> {
+        Err(ErrorKind::BitcoinCodeDeserialize(self).into())
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ScriptSerializeError {
+    PushTooLarge,
+    InvalidInteger,
+    UnknownOpcode,
+}
+
+impl ScriptSerializeError {
+    pub fn into_err<T>(self) -> Result<T> {
+        Err(ErrorKind::ScriptSerialize(self).into())
+    }
+}
 
 #[derive(Debug, ErrorChain)]
 pub enum ErrorKind {
@@ -15,13 +44,19 @@ pub enum ErrorKind {
     Utf8(std::str::Utf8Error),
 
     #[error_chain(custom)]
-    #[error_chain(description = |_| "invalid hash size")]
-    #[error_chain(display = |t: &(_, _)| write!(f, "invalid hash size, expected {}, got {}", t.0, t.1))]
-    InvalidHashSize((usize, usize)),
+    #[error_chain(description = |_| "invalid size")]
+    #[error_chain(display = |t: &(_, _)| write!(f, "invalid size, expected {}, got {}", t.0, t.1))]
+    InvalidSize((usize, usize)),
 
     #[error_chain(custom)]
     InvalidCashAddr(CashAddrError),
 
     #[error_chain(custom)]
     BitcoinCodeDeserialize(BitcoinCodeError),
+
+    #[error_chain(custom)]
+    ScriptSerialize(ScriptSerializeError),
+
+    #[error_chain(custom)]
+    InsufficientInputAmount(u64),
 }
