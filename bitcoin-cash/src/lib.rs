8522f775cad0bcf68e434f54201c9a2a57367871
tobiasruck <ruck.tobias@gmail.com>
2020-01-19 13:27:59 +0100

    Added dependencies and files to lib.rs

--- bitcoin-cash/src/lib.rs
+++ bitcoin-cash/src/lib.rs
@@ -2,13 +2,31 @@
 extern crate derive_error_chain;
 
 pub mod address;
-pub mod deserialize;
+pub mod deserializer;
 mod encoding_utils;
 pub mod error;
 mod hash;
-pub mod serialize;
+pub mod serializer;
+mod tx_builder;
+mod tx;
+mod script;
+mod tx_preimage;
+mod scripts;
+mod pubkey;
+mod crypto;
 
 pub use address::{Address, AddressType, Prefix};
-pub use deserialize::decode_bitcoin_code;
+pub use deserializer::decode_bitcoin_code;
 pub use hash::*;
-pub use serialize::encode_bitcoin_code;
+pub use serializer::*;
+pub use script::*;
+pub use tx::*;
+pub use tx_builder::*;
+pub use tx_preimage::*;
+pub use scripts::*;
+pub use pubkey::*;
+pub use crypto::*;
+
+pub use bitcoin_cash_script as ops;
+pub use bitcoin_cash_script_macro::script;
+pub use bitcoin_cash_script::ByteArray;
