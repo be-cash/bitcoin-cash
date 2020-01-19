18f8df0a63dc25e89c9e4673bba1269fea63de07
tobiasruck <ruck.tobias@gmail.com>
2020-01-19 13:16:45 +0100

    Added script.rs

---
+++ bitcoin-cash/src/script.rs
@@ -0,0 +1,202 @@
+use crate::ops::{Op, OpcodeType, Ops, encoding_utils::{encode_int}};
+use std::borrow::Cow;
+use serde::{Serializer, Serialize, Deserialize, Deserializer, ser, de};
+use byteorder::{WriteBytesExt, ReadBytesExt, LittleEndian};
+use crate::error::{Result, ScriptSerializeError};
+use std::io::Read;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Script<'a> {
+    ops: Cow<'a, [Op]>,
+}
+
+impl Ops for Script<'_> {
+    fn ops(&self) -> Cow<[Op]> {
+        self.ops.as_ref().into()
+    }
+}
+
+impl<'a> Script<'a> {
+    pub fn new(ops: Cow<'a, [Op]>) -> Self {
+        Script { ops }
+    }
+}
+
+impl<'a> Script<'a> {
+    pub fn to_script_code(&self, n_codesep: Option<usize>) -> Script<'_> {
+        let idx = if let Some(n_codesep) = n_codesep {
+            let mut n_codeseps_found = 0;
+            let mut codesep_idx = None;
+            for (idx, op) in self.ops.iter().enumerate() {
+                match op {
+                    Op::Code(OpcodeType::OP_CODESEPARATOR) => {
+                        if n_codesep == n_codeseps_found {
+                            codesep_idx = Some(idx);
+                            break;
+                        }
+                        n_codeseps_found += 1;
+                    }
+                    _ => continue,
+                }
+            }
+            codesep_idx.expect("Couldn't find OP_CODESEPARATOR")
+        } else {
+            0
+        };
+        Script::new(self.ops[idx..].as_ref().into())
+    }
+
+    pub fn to_script_code_first(&self) -> Script<'_> {
+        if let Some(code_separator_idx) = self.ops.iter().position(
+            |op| op == &Op::Code(OpcodeType::OP_CODESEPARATOR)
+        ) {
+            Script::new(self.ops[code_separator_idx + 1..].as_ref().into())
+        } else {
+            self.clone()
+        }
+    }
+
+    pub fn to_owned_script(&self) -> Script<'static> {
+        Script { ops: self.ops.clone().into_owned().into() }
+    }
+}
+
+fn serialize_push_bytes(vec: &mut Vec<u8>, bytes: &[u8]) -> Result<()> {
+    use OpcodeType::*;
+    match bytes.len() {
+        len @ 0x00..=0x4b => vec.push(len as u8),
+        len @ 0x4c..=0xff => {
+            vec.push(OP_PUSHDATA1 as u8);
+            vec.push(len as u8);
+        },
+        len @ 0x100..=0xffff => {
+            vec.push(OP_PUSHDATA2 as u8);
+            vec.write_u16::<LittleEndian>(len as u16).unwrap();
+        },
+        len @ 0x10000..=0xffff_ffff => {
+            vec.push(OP_PUSHDATA4 as u8);
+            vec.write_u32::<LittleEndian>(len as u32).unwrap();
+        },
+        _ => return ScriptSerializeError::PushTooLarge.into_err()
+    }
+    vec.extend(bytes);
+    Ok(())
+}
+
+pub fn serialize_op(vec: &mut Vec<u8>, op: &Op) -> Result<()> {
+    use OpcodeType::*;
+    match *op {
+        Op::Code(opcode) => Ok(vec.push(opcode as u8)),
+        Op::PushBoolean(boolean) => {
+            vec.push(
+                if boolean {
+                    OP_0 as u8
+                } else {
+                    OP_1 as u8
+                }
+            );
+            Ok(())
+        },
+        Op::PushInteger(int) => {
+            vec.push(
+                match int {
+                    -1 => OP_1NEGATE as u8,
+                    0 => OP_0 as u8,
+                    1..=16 => OP_1 as u8 + int as u8 - 1,
+                    -0x8000_0000 => return ScriptSerializeError::InvalidInteger.into_err(),
+                    _ => return serialize_push_bytes(vec, &encode_int(int)),
+                }
+            );
+            Ok(())
+        },
+        Op::PushByteArray(ref array) => {
+            serialize_push_bytes(vec, &array.data)
+        },
+    }
+}
+
+pub fn serialize_ops(ops: &[Op]) -> Result<Vec<u8>> {
+    let mut vec = Vec::new();
+    for op in ops {
+        serialize_op(&mut vec, op)?;
+    }
+    Ok(vec)
+}
+
+pub fn deserialize_ops(bytes: &[u8]) -> Result<Vec<Op>> {
+    use OpcodeType::*;
+    let mut i = 0;
+    let mut ops = Vec::new();
+    let mut cur = std::io::Cursor::new(bytes);
+    while i < bytes.len() {
+        let byte = cur.read_u8()?;
+        i += 1;
+        let push_len = match byte {
+            0 => {
+                ops.push(Op::Code(OP_0));
+                continue;
+            },
+            push_len @ 0x01..=0x4b => push_len as usize,
+            byte if byte == OP_PUSHDATA1 as u8 => {
+                i += 1;
+                cur.read_u8()? as usize
+            },
+            byte if byte == OP_PUSHDATA2 as u8 => {
+                i += 2;
+                cur.read_u16::<LittleEndian>()? as usize
+            },
+            byte if byte == OP_PUSHDATA4 as u8 => {
+                i += 4;
+                cur.read_u32::<LittleEndian>()? as usize
+            },
+            opcode => {
+                let opcode = num::FromPrimitive::from_u8(opcode)
+                    .ok_or(())
+                    .or_else(|()| ScriptSerializeError::UnknownOpcode.into_err())?;
+                ops.push(Op::Code(opcode));
+                continue;
+            },
+        };
+        let mut vec = vec![0; push_len];
+        cur.read_exact(&mut vec)?;
+        i += push_len;
+        ops.push(Op::PushByteArray(vec.into()));
+    }
+    Ok(ops)
+}
+
+impl<'a> Serialize for Script<'a> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error> {
+        use ser::Error;
+        serializer.serialize_bytes(
+            &serialize_ops(&self.ops).map_err(|err| S::Error::custom(err.to_string()))?
+        )
+    }
+}
+
+struct ScriptVisitor;
+
+impl<'de> de::Visitor<'de> for ScriptVisitor {
+    type Value = Vec<Op>;
+    fn expecting(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(fmt, "a byte array")
+    }
+
+    fn visit_bytes<E: de::Error>(self, v: &[u8]) -> std::result::Result<Self::Value, E> {
+        deserialize_ops(v).map_err(|err| E::custom(err.to_string()))
+    }
+}
+
+impl<'de, 'a> Deserialize<'de> for Script<'a> {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        Ok(Script::new(deserializer.deserialize_bytes(ScriptVisitor)?.into()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+}
