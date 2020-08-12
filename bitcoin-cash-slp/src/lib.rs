671ecc9aaf5f4383b0be0022a0205e7971e89efd
tobiasruck <ruck.tobias@gmail.com>
2020-08-12 20:54:27 +0200

    Added `slp_genesis_output`.

--- bitcoin-cash-slp/src/lib.rs
+++ bitcoin-cash-slp/src/lib.rs
@@ -30,6 +30,43 @@ pub fn slp_amount_ops<'a>(output_amounts: impl IntoIterator<Item = &'a u64>) -> 
         .collect()
 }
 
+pub fn slp_genesis_output(
+    slp_token_type: SlpTokenType,
+    token_ticker: &str,
+    token_name: &str,
+    token_document_url: &str,
+    token_document_hash: &str,
+    decimals: u8,
+    mint_baton_vout: Option<u8>,
+    initial_token_mint_quantity: u64,
+) -> TxOutput {
+    let byte_arrays = vec![
+        ByteArray::from_slice("lokad_id", b"SLP\0"),
+        ByteArray::new("token_type", vec![slp_token_type as u8]),
+        ByteArray::new("transaction_type", SlpTxType::GENESIS.to_string().into_bytes()),
+        ByteArray::new("token_ticker", token_ticker.as_bytes().to_vec()),
+        ByteArray::new("token_name", token_name.as_bytes().to_vec()),
+        ByteArray::new("token_document_url", token_document_url.as_bytes().to_vec()),
+        ByteArray::new("token_document_hash", token_document_hash.as_bytes().to_vec()),
+        ByteArray::new("decimals", decimals.to_be_bytes().as_ref()),
+        ByteArray::new("mint_baton_vout", mint_baton_vout.map(|vout| vout.to_be_bytes().to_vec()).unwrap_or(vec![])),
+        ByteArray::new("initial_token_mint_quantity", initial_token_mint_quantity.to_be_bytes().as_ref()),
+    ];
+    let mut ops = vec![
+        Op::Code(Opcode::OP_RETURN),
+    ];
+    for byte_array in byte_arrays {
+        ops.push(Op::PushByteArray {
+            array: byte_array,
+            is_minimal: false,
+        });
+    }
+    TxOutput {
+        value: 0,
+        script: Script::new(ops.into_iter().map(TaggedOp::from_op).collect::<Vec<_>>()),
+    }
+}
+
 pub fn slp_send_output(
     slp_token_type: SlpTokenType,
     token_id: &TokenId,
