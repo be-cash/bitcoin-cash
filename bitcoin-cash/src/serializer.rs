1c4fd8cb11d24c7a5db3197455b60dac877b36c3
tobiasruck <ruck.tobias@gmail.com>
2020-12-07 17:32:49 +1000

    Make `encode_bitcoin_code_all` infallible

--- bitcoin-cash/src/serializer.rs
+++ bitcoin-cash/src/serializer.rs
@@ -1,13 +1,12 @@
-use crate::error;
 use crate::{BitcoinCode, ByteArray};
 
 pub fn encode_bitcoin_code_all<'a, T: 'a>(
     values: impl IntoIterator<Item = &'a T>,
-) -> error::Result<ByteArray>
+) -> ByteArray
 where
     T: BitcoinCode,
 {
-    Ok(ByteArray::from_parts(
+    ByteArray::from_parts(
         values.into_iter().map(|value| value.ser()),
-    ))
+    )
 }
